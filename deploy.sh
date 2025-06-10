4290efc8760befcafdc7503237091cac10ca7929
zhangzhang88 <76701635@qq.com>
2025-06-11 07:58:55 +0800

    fix: remove Docker installation from deployment script

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,6 @@ set -e
 
 echo "开始部署..."
 
-# 安装必要的软件
-echo "正在安装 Docker 和 Docker Compose..."
-curl -fsSL https://get.docker.com | sh
-
-# 安装 Docker Compose
-curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
 # 如果已存在项目目录，则删除
 if [ -d "twitter-video-downloader" ]; then
     echo "删除旧的项目目录..."
